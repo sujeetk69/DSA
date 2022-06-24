f0f09fc6c97cc2d3def829a56abe429f81a2989e
Sujeet Kumar <sujeetk69@gmail.com>
2022-06-24 23:44:15 +0100

    Implement BFS and general refactor

--- Tree/BinaryTree.py
+++ Tree/BinaryTree.py
@@ -2,17 +2,68 @@ from typing import Any, List
 
 
 class Stack:
+    """
+    Basic implementation of a Stack data structure with minimal methods as required for Tree algorithms
+    Underlying data structure is a python list
+    """
     def __init__(self):
+        """
+        Constructor: creates an instance of this class
+        """
         self.list: List = list()
 
     def push(self, data: Any) -> None:
+        """
+        Pushes the object unto the stack
+        :param data:
+        :return:
+        """
         self.list.append(data)
 
     def pop(self) -> Any:
+        """
+        Removes and return the object from top of the list
+        Warning: when the stack is empty it will raise IndexError
+        I did not handle it intentionally
+        :return:
+        """
         return self.list.pop()
 
     def peek(self) -> Any:
+        """
+        Returns the current object on the top of the stack but does not removes it
+        :return:
+        """
         return self.list[-1]
+
+    def is_empty(self) -> bool:
+        return len(self.list) == 0
+
+
+class Queue:
+    """
+    Basic implementation of a Queue data structure with minimal methods as required for Tree algorithms
+    """
+    def __init__(self):
+        """
+        Constructor: create an instance of Queue
+        """
+        self.list = list()
+
+    def enqueue(self, data: Any) -> None:
+        """
+        Insert an object at the tail of the queue
+        :param data:
+        :return:
+        """
+        self.list.insert(0, data)
+
+    def dequeue(self) -> Any:
+        """
+        Removes and returns the object on the front of the queue
+        :return:
+        """
+        return self.list.pop()
 
     def is_empty(self) -> bool:
         return len(self.list) == 0
@@ -37,12 +88,14 @@ def inorder(root_node: Node) -> None:
     :param root_node: Root Node
     :return:
     """
+    print(f"Inorder traversal of a binary tree")
     if root_node is None:
         return
     current = root_node
     inorder(current.left_child)
     print(current.value, end=" ")
     inorder(current.right_child)
+    print()
 
 
 def inorder_non_recursive(root_node: Node) -> None:
@@ -51,6 +104,7 @@ def inorder_non_recursive(root_node: Node) -> None:
     :param root_node:
     :return:
     """
+    print(f"Inorder traversal of a binary tree")
     if root_node is None:
         return
     stack = Stack()
@@ -65,6 +119,20 @@ def inorder_non_recursive(root_node: Node) -> None:
             current = current.right_child
         else:
             break
+    print()
+
+
+def bfs(root_node: Node) -> None:
+    print(f"Breadth First Search")
+    queue = Queue()
+    queue.enqueue(root_node)
+    while not queue.is_empty():
+        current = queue.dequeue()
+        if current:
+            print(current.value, end=" ")
+            queue.enqueue(current.left_child)
+            queue.enqueue(current.right_child)
+    print()
 
 
 if __name__ == '__main__':
@@ -76,4 +144,5 @@ if __name__ == '__main__':
     root.left_child.right_child = Node(5)
     # inorder(root)
     inorder_non_recursive(root)
+    bfs(root)
     # print('PyCharm')
